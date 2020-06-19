36e11429f76090c0e9530194c4069cf6bd028dc8
junwoo45 <sinyoorang@gmail.com>
2020-06-19 12:18:01 +0900

    test: 스파이 테스트 함수의 결과값이 맞는지 테스트 - toHaveBeenCalled 매처는 단지 함수가 호출되었는지만 검사한다.

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -2,17 +2,18 @@ describe('Hello모듈의', () => {
   describe('hello는', () => {
     it('인사 문자열을 반환한다', () => {
       expect(Hello.hello).toBe('hello.');
-    })
-  })
+    });
+  });
+
   describe('greeting함수는', () => {
     it('getName함수를 호출한다', () => {
-      // const expectedStr = Hello.message;
-      // const actualStr = Hello.greeting();
-
-      // expect(actualStr).toBe(expectedStr);
       spyOn(Hello, 'getName');
       Hello.greeting();
       expect(Hello.getName).toHaveBeenCalled();
+    });
+
+    it('message와 값이 같아야 한다', () => {
+      expect(Hello.greeting()).toBe(Hello.message)
     })
-  })
+  });
 });
