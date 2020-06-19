c801ddd96a3a2d1fe21b3fa277407d06645da2f8
junwoo45 <sinyoorang@gmail.com>
2020-06-19 12:06:04 +0900

    test: 스파이 테스트 추가

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
 const Hello = {
+  hello: 'hello.',
   message: 'hello. my name is junwoo',
   greeting() {
-    return this.message;
+    return this.hello + ' ' + this.getName();
+  },
+  getName() {
+    return 'my name is junwoo';
   }
 };
--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,10 +1,18 @@
-describe('Hello', () => {
-  describe('greeting', () => {
-    it('인사를 한다.', () => {
-      const expectedStr = Hello.message;
-      const actualStr = Hello.greeting();
+describe('Hello모듈의', () => {
+  describe('hello는', () => {
+    it('인사 문자열을 반환한다', () => {
+      expect(Hello.hello).toBe('hello.');
+    })
+  })
+  describe('greeting함수는', () => {
+    it('getName함수를 호출한다', () => {
+      // const expectedStr = Hello.message;
+      // const actualStr = Hello.greeting();
 
-      expect(actualStr).toBe(expectedStr);
+      // expect(actualStr).toBe(expectedStr);
+      spyOn(Hello, 'getName');
+      Hello.greeting();
+      expect(Hello.getName).toHaveBeenCalled();
     })
   })
 });
